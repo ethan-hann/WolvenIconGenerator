368952e96794371bbbedd676db33f2e18a340375
Ethan Hann <36464732+ethan-hann@users.noreply.github.com>
2024-09-19 22:44:50 -0500

    Update WolvenKitVersion to stable release 8.14.0
    The WolvenKitVersion property in the Header class has been updated
    from "8.13.0-nightly.2024-03-17" to "8.14.0". This change signifies
    a transition from a nightly build to a stable release version.

--- WIG.Lib/Tools/InkAtlas/Header.cs
+++ WIG.Lib/Tools/InkAtlas/Header.cs
@@ -2,7 +2,7 @@
 
 internal class Header
 {
-    public string WolvenKitVersion { get; set; } = "8.13.0-nightly.2024-03-17";
+    public string WolvenKitVersion { get; set; } = "8.14.0";
     public string WKitJsonVersion { get; set; } = "0.0.8";
     public int GameVersion { get; set; } = 2120;
     public string ExportedDateTime { get; set; } = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ");
